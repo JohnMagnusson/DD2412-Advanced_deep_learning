fe3bc484f96f4f4dad8cae4935089e981cfd0701
matteo <demartis@kth.se>
2020-10-09 15:14:27 +0200

    ResNet-18 implementation (to be tested)

---
+++ src/models/resnet18.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import tensorflow.keras
+from tensorflow.keras.layers import Dense, Conv2D
+from tensorflow.keras.layers import BatchNormalization, Activation
+from tensorflow.keras.layers import GlobalAveragePooling2D, Input, Flatten, MaxPool2D, AveragePooling2D
+from tensorflow.keras.models import Model
+
+
+
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True):
+
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same')
+
+    x = inputs
+
+    x = conv(x)
+    if batch_normalization:
+        x = BatchNormalization()(x)
+    if activation is not None:
+        x = Activation(activation)(x)
+
+    return x
+
+
+def resnet18(input_shape, num_classes):
+    """ResNet-18
+
+    -> Arguments
+        input_shape (tensor): shape of input image tensor
+        num_classes (int): number of classes
+
+    -> Returns
+        model (Model): Tensorflow model instance
+    """
+
+    num_filters = 64
+    inputs = Input(shape=input_shape)
+
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters,
+                     kernel_size=(7, 7))
+
+    x = MaxPool2D(pool_size=(3, 3),
+                  strides=2,
+                  padding="same")(x)
+    # Instantiate the stack of residual units
+    num_res_blocks = [3, 4, 6, 3]
+    for stack in range(4):
+        for res_block in range(num_res_blocks[stack]):
+            strides = 1
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                strides = 2  # downsample
+            y = resnet_layer(inputs=x,
+                             kernel_size=(3, 3),
+                             num_filters=num_filters,
+                             strides=strides)
+            y = resnet_layer(inputs=y,
+                             kernel_size=(3, 3),
+                             num_filters=num_filters,
+                             activation=None)
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 kernel_size=(1, 1),
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=True)
+            x = tensorflow.keras.layers.add([x, y])
+            x = Activation('relu')(x)
+        num_filters *= 2
+
+    # x = GlobalAveragePooling2D()(x)
+    # y = Flatten()(x)
+    x = GlobalAveragePooling2D()(x)
+    # y = Flatten()(x)
+
+    outputs = Dense(num_classes,
+                    activation='softmax')(x)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
