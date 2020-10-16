0fa7bce3049000fa65126a60c5a820d0a8a24dc4
matteodemartis <matteo.demartis.b51s@gmail.com>
2020-10-16 17:33:38 +0200

    LARS Optimizer implemented

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from modelFunctions import train_model, evaluate_model, build_simCLR_model, buil
 
 
 train_data, test_data = get_data_set()
-model = build_simCLR_model(encoder_network="resnet-18", projection_head_mode="linear")
+model = build_simCLR_model(encoder_network="resnet-18", projection_head_mode="nonlinear")
 # model = build_normal_resnet()
 
 trained_model, trainingStats = train_model(model, train_data, test_data)
