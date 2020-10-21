808a93da8c6252aa9dd2957e7038d89af5f77cb2
matteodemartis <matteo.demartis.b51s@gmail.com>
2020-10-21 14:56:28 +0000

    resnet50 set up for training

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from modelFunctions import *
 from models.resnet50 import ResNet50
 import flagSettings
 
-do_warmup_new_model = False
+do_warmup_new_model = True
 do_train_new_model = True
-do_fine_tune_model = False
+do_fine_tune_model = True
 do_evaluation_on_model = False
 
 
@@ -14,31 +14,31 @@ train_data, val_data, test_data = get_data_set()
 
 if do_warmup_new_model:
     print("Starting warmup")
-    model = build_simCLR_model(encoder_network="resnet-18", projection_head_mode="nonlinear")
+    model = build_simCLR_model(encoder_network="resnet-50", projection_head_mode="nonlinear")
     trained_model, training_loss, validation_loss = warmup_model(model, train_data, val_data)
     plot_loss(training_loss, validation_loss)
-    trained_model.save_weights("warmedup_models/simCLR_model_weights_3_warmedup")
+    trained_model.save_weights("warmedup_models/simCLR_model_weights_resnet50_warmedup")
     print("Done with warmup")
 
 if do_train_new_model:
     print("Starting real training")
     model = build_simCLR_model(encoder_network="resnet-50", projection_head_mode="nonlinear")
-    #model.load_weights("warmedup_models/simCLR_model_weights_3_warmedup")
+    model.load_weights("warmedup_models/simCLR_model_weights_resnet50_warmedup")
     trained_model, training_loss, validation_loss = train_model(model, train_data, val_data)
     plot_loss(training_loss, validation_loss)
-    trained_model.save_weights("saved_models/simCLR_model_weights_3")
+    trained_model.save_weights("saved_models/simCLR_model_weights_resnet50")
     print("Done with training")
 
 if do_fine_tune_model:
     print("Starting with fine-tuning")
-    model = build_simCLR_model(encoder_network="resnet-18", projection_head_mode="nonlinear")
-    model.load_weights("saved_models/simCLR_model_weights_3")
+    model = build_simCLR_model(encoder_network="resnet-50", projection_head_mode="nonlinear")
+    model.load_weights("saved_models/simCLR_model_weights_resnet50")
     train_data_sub = balanced_subsample(train_data, flagSettings.percentage_fine_tune_data)
     validation_data_sub = balanced_subsample(val_data, flagSettings.percentage_fine_tune_data)
 
     fine_tuned_model, history_fine_tune = fine_tune_model(model, "nonlinear", train_data_sub, validation_data_sub)
     plot_fine_tuning(history_fine_tune)
-    fine_tuned_model.save_weights("finetuned_models/simCLR_model_weights_3")
+    fine_tuned_model.save_weights("finetuned_models/simCLR_model_weights_resnet50")
     print("Done with fine-tuning")
 
 
